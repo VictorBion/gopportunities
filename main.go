7ad31d33d33b6c627b4c3d5b33f67b525cc8d253
vitor <vitorbion@gmail.com>
2025-10-31 04:10:13 -0300

    feat: add opening schema package and custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/VictorBion/gopportunities/router"
+import (
+	"github.com/VictorBion/gopportunities/config"
+	"github.com/VictorBion/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main") 
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 
+	// Initialize Router
 	router.Initializer()
 }
