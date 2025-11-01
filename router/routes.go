06eee9c240d2c5ae1f2cdb44d8efa5fbbc6264cb
vitor <vitorbion@gmail.com>
2025-11-01 01:44:44 -0300

    feat: add request and response helpers

--- router/routes.go
+++ router/routes.go
@@ -6,17 +6,19 @@ import (
 )
 
 func initializerRoutes(router *gin.Engine) {
+	// Initialize Handler
+	handler.InitializeHandler()
 
 	v1 := router.Group("/api/v1")
 	{
-	v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
 
-	v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
 
-	v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-	v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 
-	v1.GET("/openings", handler.ListOpeningsHandler)
-}	
+		v1.GET("/openings", handler.ListOpeningsHandler)
+	}
 }
